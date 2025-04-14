53eb9b4537742c9c8f9883f4401285d29cbf775e
Abhilov23 <abhilovgupta01@gmail.com>
2025-04-14 15:26:23 +0530

    created test cases

--- test/BasicNft.t.sol
+++ test/BasicNft.t.sol
@@ -9,7 +9,9 @@ import {BasicNFT} from "../src/BasicNFT.sol";
 
 contract BasicNftTest is Test{
     DeployBasicNft public deployer;
+    address public USER = makeAddr("user");
     BasicNFT public basicNft;
+    string public constant PUG ="ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
 
     function setUp() public{
         deployer = new DeployBasicNft();
@@ -17,9 +19,17 @@ contract BasicNftTest is Test{
     }
 
     function testNameIsCorrect() public view{
-        string memory expectedName = "Doggy";
+        string memory expectedName = "DOGGY";
         string memory actualName = basicNft.name();
         //comparing two strings by encoding them
         assert(keccak256(abi.encodePacked(expectedName)) == keccak256(abi.encodePacked(actualName)));
+    }
+
+    function testCanMintAndHaveBalance() public {
+        vm.prank(USER);
+        basicNft.mintNFT(PUG);
+
+        assert(basicNft.balanceOf(USER)==1);
+        assert(keccak256(abi.encodePacked(PUG)) == keccak256(abi.encodePacked(basicNft.tokenURI(0))));
     }
 }
